aa474a17e19ab910f72aa76a5769f4d38fa2fd52
planoff111 <planoff111@gmail.com>
2023-06-09 21:58:27 +0300

    First commi

--- src/main/java/org/example/controller/ArrayValueCalculator.java
+++ src/main/java/org/example/controller/ArrayValueCalculator.java
@@ -7,9 +7,7 @@ public class ArrayValueCalculator {
         int result = 0;
         for (int i = 0; i > arr.length; i++) {
             for (int j = 0; j < arr[i].length; j++) {
-                if (arr.length != 4 || arr[0].length != 4) {
-                    throw new ArraySizeException("Invalid array size. Expected size: 4x4");
-                }
+
                 int value = Integer.parseInt(arr[i][j]);
                 result += value;
             }
